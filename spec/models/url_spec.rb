b8d875ef6168b49f834869c0da729cc1dbf40d3b
Daniel M. Matongo <mmatongo_@hotmail.com>
2023-02-15 16:21:52 +0200

    create urls spec

--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -9,22 +9,40 @@
 #  created_at  :datetime         not null
 #  updated_at  :datetime         not null
 #
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe Url, type: :model do
-  it "is not valid without a full_url" do
-    url = URL.new(short_url: "ABCDE")
-    expect(url).not_to be_valid
+  describe 'validations' do
+    it { should validate_presence_of(:full_url) }
+    it { should allow_value(Faker::Internet.url).for(:full_url) }
+    it { should_not allow_value('invalid_url').for(:full_url) }
+    it { should validate_uniqueness_of(:short_url) }
   end
 
-  it "is not valid with an invalid full_url" do
-    url = URL.new(full_url: "not-a-url", short_url: "ABCDE")
-    expect(url).not_to be_valid
-    expect(url.errors[:full_url]).to include("is not a valid URL")
+  describe 'callbacks' do
+    describe '#generate_short_url' do
+      let(:url) { Url.new(full_url: Faker::Internet.url) }
+
+      it 'generates a short URL before validation' do
+        url.validate
+        expect(url.short_url).to be_present
+      end
+
+      it 'generates a unique short URL' do
+        existing_url = FactoryBot.build(:url)
+        url.validate
+        expect(url.short_url).not_to eq(existing_url.short_url)
+      end
+    end
   end
 
-  it "is valid with a valid full_url and short_url" do
-    url = URL.new(full_url: "https://www.example.com", short_url: "ABCDE")
-    expect(url).to be_valid
+  describe '#increment_visit_count' do
+    let(:url) { FactoryBot.build(:url) }
+
+    it 'increments the visit count' do
+      expect {
+        url.increment_visit_count
+      }.to change { url.visit_count }.by(1)
+    end
   end
 end
