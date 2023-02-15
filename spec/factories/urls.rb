65c04db12262c28a86a79090013262b9cfbfbfce
Daniel M. Matongo <mmatongo_@hotmail.com>
2023-02-15 18:29:45 +0200

    create factory

--- spec/factories/urls.rb
+++ spec/factories/urls.rb
@@ -1,10 +1,5 @@
 FactoryBot.define do
   factory :url do
     full_url { Faker::Internet.url }
-    short_url { SecureRandom.alphanumeric(5).upcase }
-  end
-
-  factory :visit do
-    url
   end
 end
