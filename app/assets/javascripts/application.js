1ae04a5f720e9a6f28312fc8489e62399f88750d
Daniel M. Matongo <mmatongo_@hotmail.com>
2023-02-17 21:54:57 +0200

    asset update

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,1 +1,24 @@
-//= require urls
+function updateVisitCount(urlId) {
+  const url = `/urls/${urlId}/visit`;
+  const authenticity_token = document
+    .querySelector('meta[name="csrf-token"]')
+    .getAttribute("content");
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "X-CSRF-Token": authenticity_token,
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    credentials: "same-origin",
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      const visitCountEls = document.querySelectorAll(`#visit-count-${urlId}`);
+      visitCountEls.forEach((el) => {
+        el.innerHTML = data.visitCount;
+      });
+    })
+    .catch((error) => console.error(error));
+}
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -6,7 +6,6 @@ Rails.application.config.assets.version = "1.0"
 # Add additional assets to the asset load path.
 # Rails.application.config.assets.paths << Emoji.images_path
 # add the urls.js file to the asset pipeline
-Rails.application.config.assets.precompile += %w( urls.js )
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
